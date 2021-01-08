37cb03a4c4bda29b7cca9c3040e6271868116235
Rob Phoenix <rphoenix@uw.co.uk>
2021-01-08 12:57:55 +0000

    feat: add docs components

---
+++ docs/components.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import Box, { BoxProps } from '@material-ui/core/Box'
+
+const Wrapper: React.FC = (props) => (
+  <Box sx={{ p: 4, backgroundColor: 'grey.100' }} {...props} />
+)
+
+const Item: React.FC<Boxprops> = (props) => {
+  const { sx, ...rest } = props
+  return (
+    <Box
+      sx={{ height: '2rem', backgroundColor: 'secondary.light', ...sx }}
+      {...rest}
+    />
+  )
+}
+
+export { Wrapper, Item }
